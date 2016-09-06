44553c539359a64681095fe99c20640de2733e64
di <stevejobsii@outlook.com>
2016-09-06 15:30:09 +0800

    mail config

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'Zhu@goodgoto.com'),
+        'address' => env('MAIL_FROM_ADDRESS', '1@goodgoto.com'),
         'name'    => env('MAIL_FROM_NAME', 'Zhu'),
     ],
 
