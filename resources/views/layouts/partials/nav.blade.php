5e1204aebe1c9951dbed1ad895fc34d7878d9d14
di <stevejobsii@outlook.com>
2016-10-07 19:45:04 +0800

    no search

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -17,11 +17,11 @@
       </ul>
 
       <div class="navbar-right">
-          <form method="GET" action="{{route('search')}}" accept-charset="UTF-8" class="navbar-form navbar-left" target="_blank">
+          <!-- <form method="GET" action="{{route('search')}}" accept-charset="UTF-8" class="navbar-form navbar-left" target="_blank">
             <div class="form-group">
             <input class="form-control search-input mac-style" placeholder="{{lang('Search')}}" name="q" type="text">
             </div>
-          </form>
+          </form> -->
 
         <ul class="nav navbar-nav github-login" >
           @if (Auth::check())
